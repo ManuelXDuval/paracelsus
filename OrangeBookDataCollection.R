ef4c63eb129ce75c0c0be26c9746704a1bbd5c61
manuelseafarer <manuel.duval@sciphermedicine.com>
2019-03-24 11:45:37 -0400

    subsetting for creating a list of non redundant ingredients

--- OrangeBookDataCollection.R
+++ OrangeBookDataCollection.R
@@ -3,6 +3,7 @@
 # Authoritative source of data related to therapeutically active molecules     #
 # The source URL is https://www.fda.gov/Drugs/InformationOnDrugs/              #
 ################################################################################
+library("reshape2")
 # setting the source URL
 url.basename <- "https://www.fda.gov/downloads/Drugs/InformationOnDrugs/"
 # setting a new subdirectory on the local file system where to load the Orange  
@@ -23,6 +24,13 @@ prodDf <- read.table("LocalEOB/products.txt", header = T, sep = "~", quote = "",
                      comment.char = "", 
                      colClasses = c(rep("character", 4), rep("NULL", 1), 
                                     rep("character", 9)))
+# selecting out ANDA and Discontinued
+prodDfNDA <- prodDf[(prodDf$Appl_Type == "N" & prodDf$Type == "RX"),]
+# setting the list of ingredients
+Ingred1 <- unique(prodDfNDA$Ingredient[!grepl(";", prodDfNDA$Ingredient)])
+CompIngred <- unique(prodDfNDA$Ingredient[grepl(";", prodDfNDA$Ingredient)]) 
+CompIngredDf <- colsplit(CompIngred, "\\;", names = c("primIng", "SecondIng"))
+Ingred2 <- unique(c(Ingred1, trimws(CompIngredDf$primIng)))
 ################################################################################
 
 
