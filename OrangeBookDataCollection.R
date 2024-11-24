d1ab748730718c9b85fa016e80e9c4e9a2e140c9
Manuel Duval <Manuel.X.Duval@gmail.com>
2024-11-24 10:16:53 -0500

    minor security update

--- OrangeBookDataCollection.R
+++ OrangeBookDataCollection.R
@@ -30,10 +30,14 @@ prodDf <- read.table("LocalEOB/products.txt", header = T, sep = "~", quote = "",
                                     rep("character", 9)))
 # selecting out ANDA and Discontinued
 prodDfNDA <- prodDf[(prodDf$Appl_Type == "N" & prodDf$Type == "RX"),]
+
 # setting the list of ingredients. 
 Ingred1 <- unique(prodDfNDA$Ingredient[!grepl(";", prodDfNDA$Ingredient)])
+
 CompIngred <- unique(prodDfNDA$Ingredient[grepl(";", prodDfNDA$Ingredient)]) 
+
 CompIngredDf <- colsplit(CompIngred, "\\;", names = c("primIng", "SecondIng"))
+
 Ingred2 <- unique(c(Ingred1, trimws(CompIngredDf$primIng)))
 # the Ingred2 vector: active ingredients of the Orange Book prescription drugs
 
@@ -50,12 +54,12 @@ DelayFunction <- function(SecDelay, f) {
     f(...)
   }
 }
-# function to query PubChem Compound DB with Ingredient Name to get their CID
+# function set to query PubChem Compound DB with Ingredient Name to get their CID
 QueryPubChem4CID <- function(x){
   gsub("\n", ";", trimws(
     getURL(paste0(prolog, input, gsub(" ", "%20", x),"/cids/TXT"))))
 }
-# issuing the https get requests
+# getting CID attribute value from the PubChem resource
 paracelsusDf <- as.data.frame(
   cbind(Ingred2, sapply(Ingred2, DelayFunction(.6, QueryPubChem4CID))))
 colnames(paracelsusDf) <- c("Ingredient", "CID")
@@ -80,7 +84,7 @@ tokensPerIngredient <- sapply(1:length(ingredients),
 paracelsusDf <- paracelsusDf[!(paracelsusDf$Ingredient %in%
                                  ingredients[which(tokensPerIngredient > 3)]),]
 
-# extracting actual API string value from the salt forms
+# extracting actual API string value from the salt forms  
 # 1st, selecting ingredients with a single string character value
 singleTokenIngredient <- ingredients[which(tokensPerIngredient == 1)]
 # select ingredients that are associated with another string
@@ -91,6 +95,7 @@ OneTokenIngredient <- setdiff(singleTokenIngredient,
                                         unlist(
                                           str_split(
                                             multiTokenIngredient, " "))))
+
 
 IngredientsWithSalt <- sapply(
   1:length(
@@ -104,7 +109,7 @@ IngredientsWithSalt <- sapply(
     ))
 
 names(IngredientsWithSalt) <- setdiff(singleTokenIngredient, OneTokenIngredient)
-IngredientsWithSalt <- IngredientsWithSalt[lapply(IngredientsWithSalt, length)>0]
+IngredientsWithSalt <- IngredientsWithSalt[lapply(IngredientsWithSalt, length) > 0]
 
 # list of ingredients and ingredients with salt form
 IngredientsWithSalt <- lapply(
@@ -112,37 +117,47 @@ IngredientsWithSalt <- lapply(
     as.data.frame(names(IngredientsWithSalt[i])), 
     as.data.frame(multiTokenIngredient[unlist(IngredientsWithSalt[[i]])])))
 
-IngredientsWithSalt <- lapply(IngredientsWithSalt, setNames, c("Ingredient", "SaltForm"))
+
+IngredientsWithSalt <- lapply(IngredientsWithSalt, 
+                              setNames, c("Ingredient", "SaltForm"))
+
 IngredientsWithSalt.df <- do.call("rbind", IngredientsWithSalt)
 
 IngredientsWithSalt.df <- IngredientsWithSalt.df %>%
   group_by(Ingredient) %>%
  dplyr::summarise(SaltForms = paste(SaltForm, collapse = ", "))
 
-IngredientsWithSalt.df <- merge(IngredientsWithSalt.df, paracelsusDf, by = "Ingredient")
+IngredientsWithSalt.df <- merge(IngredientsWithSalt.df, 
+                                paracelsusDf, by = "Ingredient")
 
 # dataframe with 506 entries by 3 columns (Ingredient name, CID and salt form 
 # when applicable)
 paracelsusDf <- rbind.fill(
-  paracelsusDf[(paracelsusDf$Ingredient %in% OneTokenIngredient),], IngredientsWithSalt.df)
-
-
+  paracelsusDf[(paracelsusDf$Ingredient %in% OneTokenIngredient),], 
+  IngredientsWithSalt.df)
 
 
 ##~~NIH NLM Medical Subject Heading resource query and result set retrieval~~##
 # setting url string values for issuing queries to the MeSH API
 host <- "id.nlm.nih.gov/mesh"
 endpoint <- "/lookup/descriptor"
+
 # mapping OrangeBook Ingredient name to MeSH Unique ID
 meshSet.df <- do.call("rbind", lapply(
   1:dim(paracelsusDf)[1], function(i) as.data.frame(
     content(GET(paste0(host,endpoint,"?label=", paracelsusDf$Ingredient[i]))))
   ))
+ 
 # setting dataframe column string values
 meshSet.df$MeSH_UniqueID <- gsub("http.*./", "", meshSet.df$resource)
 meshSet.df$label <- toupper(meshSet.df$label)
+
 # creating the 3 attributes dataframe object
-paracelsusDf.mesh <- merge(paracelsusDf, meshSet.df[,c(2,3)], by.x = "Ingredient", by.y = "label")
+paracelsusDf.mesh <- merge(paracelsusDf, meshSet.df[,c(2,3)], 
+                           by.x = "Ingredient", by.y = "label")
 
 ###################################################################################
+tmp <- setdiff(paracelsusDf$Ingredient, paracelsusDf.mesh$Ingredient)
+
+
 
